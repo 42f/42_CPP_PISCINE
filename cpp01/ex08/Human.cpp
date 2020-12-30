c4b78680a726b969b083a36dbe20c6508f01c129
Brian on VM <bvalette@student.42.fr>
2020-12-30 09:01:38 +0100

    [Wed-30.12-09:01] [DEV] fixe ex08 to avoid segfault [NEXT]

--- cpp01/ex08/Human.cpp
+++ cpp01/ex08/Human.cpp
@@ -41,12 +41,12 @@ void	Human::nothingAttack( __attribute__((unused))std::string const &target) con
 }
 
 void	Human::action(std::string const &action_name, std::string const &target) {
-	std::string	const name_list[3] = { "meleeAttack", "rangedAttack", "intimidatingShout" };
+	std::string	const name_list[3] = { "meleeAttack", "rangedAttack", "intimidatingShout"};
 	void 		(Human::*function_list[4])(std::string const &) const = { &Human::meleeAttack,
 				&Human::rangedAttack, &Human::intimidatingShout, &Human::nothingAttack };
 	int			i = 0;
 
-	while (name_list[i] != action_name && i < 3)
+	while (i < 3 && name_list[i] != action_name)
 		i++;
 	(this->*function_list[i])(target);
 }
