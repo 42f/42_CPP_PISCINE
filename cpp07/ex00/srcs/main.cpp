c9c2614aa6a963e310f6d17df89e7558662ed7bf
Brian on VM <bvalette@student.42.fr>
2021-01-26 16:08:06 +0100

    [Tue-26.01-16:08] [DEV] add eval tips [NEXT]

--- cpp07/ex00/srcs/main.cpp
+++ cpp07/ex00/srcs/main.cpp
@@ -10,6 +10,24 @@
 # define RED_COLOR    "\033[31m"
 #endif
 
+class Awesome {
+	public :
+		Awesome ( void ) : _n(0) {}						// add constructor by default + accessor getValue + const keyword
+		Awesome ( int n ) : _n(n) {}
+		bool	operator==( Awesome const &rhs ) const	{return (this->_n == rhs._n); }
+		bool	operator!=( Awesome const &rhs ) const	{return (this->_n != rhs._n); }
+		bool	operator> ( Awesome const &rhs ) const	{return (this->_n >  rhs._n); }
+		bool	operator< ( Awesome const &rhs ) const	{return (this->_n <  rhs._n); }
+		bool	operator>=( Awesome const &rhs ) const	{return (this->_n >= rhs._n); }
+		bool	operator<=( Awesome const &rhs ) const	{return (this->_n <= rhs._n); }
+
+		int		getValue( void ) const { return (this->_n); }
+
+	private:
+		int _n;
+};
+
+
 int		main( void ) 	{
 
 	std::cout << RED_COLOR << "-------- [START MAIN FROM SUBJECT] -----------" << RESET_COLOR << std::endl;
@@ -33,6 +51,19 @@ int		main( void ) 	{
 		std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
 	}
 	std::cout << RED_COLOR << "-------- [END   MAIN FROM SUBJECT] -----------" << RESET_COLOR << std::endl;
+	std::cout << RED_COLOR << "-------- [Awesome class] -----------" << RESET_COLOR << std::endl;
+	{
+		Awesome a(42);
+		Awesome b(21);
 
+		Awesome c = min(a, b);
+		std::cout << "Min of (" << a.getValue() << ", " << b.getValue() << ") = " << c.getValue() << std::endl;
+		c = max(a, b);
+		std::cout << "Max of (" << a.getValue() << ", " << b.getValue() << ") = " << c.getValue() << std::endl;
+		std::cout << "[swap: before] a = " << a.getValue() << ", b = " << b.getValue() << std::endl;
+		::swap(a, b);
+		std::cout << "[swap: after ] a = " << a.getValue() << ", b = " << b.getValue() << std::endl;
+
+	}
 	return (0);
 }
